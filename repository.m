8c392ff832611f8548e3c673e045a62502e94468
test-testperson <98484229+test-testperson@users.noreply.github.com>
2023-08-09 21:30:05 -0400

    Add repository function

---
+++ repository.m
@@ -0,0 +1,5 @@
+function metadata = repository
+metadata.owner = "test-testperson";
+metadata.name = "sample-releases-archived";
+metadata.url = "https://github.com/test-testperson/sample-releases-archived";
+end
